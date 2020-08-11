38a981f112f50b24b21d2f5f6267ab4199e203f9
chihiro <fujisawachihiro@gmail.com>
2020-08-11 22:06:17 +0900

    p15

--- src/Dollar.ts
+++ src/Dollar.ts
@@ -3,6 +3,6 @@ export class Dollar {
   constructor(public amount: number){}
 
   times(multiplier: number){
-    this.amount *= multiplier;
+    return new Dollar(this.amount * multiplier);
   }
 }
