849ab9ae86ea8a920e496f9d81deb33c6a77cf2b
chihiro <fujisawachihiro@gmail.com>
2020-08-11 22:03:50 +0900

    p14

--- __tests__/Money-test.ts
+++ __tests__/Money-test.ts
@@ -4,4 +4,6 @@ import {Dollar} from '../src/Dollar'
     const five = new Dollar(5);
     five.times(2);
     expect(five.amount).toBe(10);
+    five.times(3);
+    expect(five.amount).toBe(15);
   })
