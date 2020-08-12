0a0ed0576e113ab141a91c12ff8dbe293df7242d
chihiro <fujisawachihiro@gmail.com>
2020-08-12 15:48:13 +0900

    p61

--- src/Money.ts
+++ src/Money.ts
@@ -1,17 +1,16 @@
 export abstract class Money {
-  protected currencyStr!: string;
-  constructor(protected readonly amount: number) {}
+  constructor(protected readonly amount: number, protected readonly currencyStr: string) {}
 
   equals(money: Money): boolean {
     return this.amount === money.amount && money.constructor.name === this.constructor.name;
   }
 
   static dollar(amount: number): Money {
-    return new Dollar(amount);
+    return new Dollar(amount, 'USD');
   }
 
   static franc(amount: number): Money {
-    return new Franc(amount);
+    return new Franc(amount, 'CHF');
   }
 
   abstract times(multiplier: number): Money;
@@ -22,25 +21,13 @@ export abstract class Money {
 }
 
 class Dollar extends Money {
-  
-  constructor(amount: number) {
-    super(amount);
-    this.currencyStr = 'USD'
-  }
-
   times(multiplier: number): Money {
-    return new Dollar(this.amount * multiplier);
+    return Money.dollar(this.amount * multiplier);
   }
 }
 
 class Franc extends Money {
-  
-  constructor(amount: number) {
-    super(amount);
-    this.currencyStr = 'CHF'
-  }
-
   times(multiplier: number): Money {
-    return new Franc(this.amount * multiplier);
+    return Money.franc(this.amount * multiplier)
   }
 }
