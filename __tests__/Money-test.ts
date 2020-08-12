fa5bad8fc36fcf066e96f2e220bf77f3dccf9c60
chihiro <fujisawachihiro@gmail.com>
2020-08-12 12:51:56 +0900

    p52

--- __tests__/Money-test.ts
+++ __tests__/Money-test.ts
@@ -1,23 +1,21 @@
-import {Dollar} from '../src/Dollar'
-import {Franc} from '../src/Franc'
 import { Money } from '../src/Money';
-  
+
   test('test multiplication', () => {
-    const five = Money.dollar(5);
-    expect(five.times(2)).toStrictEqual(new Dollar(10));
-    expect(five.times(3)).toStrictEqual(new Dollar(15));
+    const five: Money = Money.dollar(5);
+    expect(five.times(2)).toStrictEqual(Money.dollar(10));
+    expect(five.times(3)).toStrictEqual(Money.dollar(15));
   })
 
   test('equals()', () => {
-    expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
-    expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
-    expect(new Franc(5).equals(new Franc(5))).toBe(true);
-    expect(new Franc(5).equals(new Franc(6))).toBe(false);
-    expect(new Franc(5).equals(new Dollar(5))).toBe(false);
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
+    expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
+    expect(Money.franc(5).equals(Money.franc(5))).toBe(true);
+    expect(Money.franc(5).equals(Money.franc(6))).toBe(false);
+    expect(Money.franc(5).equals(Money.dollar(5))).toBe(false);
   })
 
   test('test Franc multiplication', () => {
-    const five = new Franc(5);
-    expect(five.times(2)).toStrictEqual(new Franc(10));
-    expect(five.times(3)).toStrictEqual(new Franc(15));
+    const five = Money.franc(5);
+    expect(five.times(2)).toStrictEqual(Money.franc(10));
+    expect(five.times(3)).toStrictEqual(Money.franc(15));
   })
--- src/Money.ts
+++ src/Money.ts
@@ -1,8 +1,40 @@
-export class Money {
+export abstract class Money {
   constructor(protected readonly amount: number){}
 
   equals(money: Money): boolean{
     return this.amount === money.amount 
       && money.constructor.name === this.constructor.name;
+  }
+
+  static dollar(amount: number): Money{
+    return new Dollar(amount)
+  }
+
+  static franc(amount: number): Money{
+    return new Franc(amount)
+  }
+
+  abstract times(multiplier: number): Money;
+}
+
+class Dollar extends Money{
+  
+  constructor(amount: number){
+    super(amount);
+  }
+
+  times(multiplier: number): Money{
+    return new Dollar(this.amount * multiplier);
+  }
+}
+
+class Franc extends Money{
+  
+  constructor(amount: number){
+    super(amount)
+  }
+
+  times(multiplier: number): Money{
+    return new Franc(this.amount * multiplier);
   }
 }
